078939a4884e7ab23497bb55d7b5457cea18204b
yohanii <kim-yohan@gim-yohan-ui-MacBookPro-2.local>
2024-07-08 14:42:27 +0900

    feat: kakao api 연결

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <button></button>
+  <button @click="getResponse">요청 보내기</button>
+  <article id="response-area">
+
+  </article>
 </template>
 
 <script>
@@ -12,7 +15,34 @@ export default {
     }
   },
   methods : {
-    example() {
+    async getResponse() {
+      const area = document.getElementById("response-area");
+
+      try {
+        const response = await fetch("https://dapi.kakao.com/v2/local/search/address.json? " + new URLSearchParams({
+          analyze_type: "similar",
+          page: 1,
+          size: 10,
+          query: "경기도 구리시"
+        }).toString(), {
+          headers : {
+            Authorization : "KakaoAK " + process.env.VUE_APP_REST_API_KEY
+          }
+        });
+
+        if (!response.ok) {
+          throw new Error("데이터를 가져오는 도중 오류 발생");
+        }
+
+        const data = await response.json();
+
+        area.textContent = JSON.stringify(data);
+
+      } catch (error) {
+        console.error("데이터를 가져오는 도중 오류 발생", error);
+      }
+
+
 
     }
   },
